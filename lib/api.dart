94ec8b243fa0fca47089d997774424ac19c32ae5
Felix Bruns <felix@bruns.hamburg>
2025-03-19 20:24:56 +0100

    include api generator config

---
+++ lib/api.dart
@@ -0,0 +1,13 @@
+// Openapi Generator last run: : 2025-03-19T20:09:06.527614
+import 'package:openapi_generator_annotations/openapi_generator_annotations.dart';
+
+@Openapi(
+  additionalProperties: AdditionalProperties(
+    sourceFolder: 'lib/api/gamevault_api',
+  ),
+  inputSpec: InputSpec(path: 'gamevault_api.json'),
+  generatorName: Generator.dio,
+  outputDirectory: 'build/gamevault_api',
+  skipSpecValidation: true,
+)
+class GvApi {}
