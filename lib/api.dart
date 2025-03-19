db333d8aee3dd1680f703fa09665ead538d8dc0f
Felix Bruns <felix@bruns.hamburg>
2025-03-19 22:47:51 +0100

    Configuration for generation

--- lib/api.dart
+++ lib/api.dart
@@ -1,13 +1,13 @@
-// Openapi Generator last run: : 2025-03-19T20:09:06.527614
+// Openapi Generator last run: : 2025-03-19T22:45:24.818796
 import 'package:openapi_generator_annotations/openapi_generator_annotations.dart';
 
 @Openapi(
   additionalProperties: AdditionalProperties(
-    sourceFolder: 'lib/api/gamevault_api',
+    sourceFolder: 'gamevault_api',
   ),
   inputSpec: InputSpec(path: 'gamevault_api.json'),
   generatorName: Generator.dio,
-  outputDirectory: 'build/gamevault_api',
+  outputDirectory: '.',
   skipSpecValidation: true,
 )
 class GvApi {}
